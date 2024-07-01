21032717b4d40cba2e24c5839bef81cc52b8dfae
Tanvi Patel <8438tanvipatel@gmail.com>
2024-07-01 11:28:41 +0530

    Add files via upload

---
+++ ternaryOperator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int minimumNumber;
+	printf("Enter the number :");
+	scanf("%d", &minimumNumber);
+	
+	(8<3)?printf("the minimum value is 8."):printf("the minimum value is 3.");
+}
