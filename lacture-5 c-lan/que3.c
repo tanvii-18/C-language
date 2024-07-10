ad31076c1d687c0997063968f7cc32b6d60e4571
Tanvi Patel <8438tanvipatel@gmail.com>
2024-07-10 12:02:41 +0530

    Add files via upload

--- lacture-5 c-lan/que3.c
+++ lacture-5 c-lan/que3.c
@@ -3,23 +3,8 @@
 
 void main()
 {
-	int average,subject1,subject2,subject3,total;
+	int average;
 	
-	printf("Enter the marks of subject 1 :");
-	scanf("%d", &subject1);
-	
-	printf("Enter the marks of subject 2 :");
-	scanf("%d", &subject2);
-	
-	printf("Enter the marks of subject 3 :");
-	scanf("%d", &subject3);
-
-	total=subject1+subject2+subject3;
-	printf("Total marks: %d\n",total);
-	
-	average=(total/3)*100;
-	printf("average marks : %d");
-	
-
+	printf("enter the number :");
 	
 }
